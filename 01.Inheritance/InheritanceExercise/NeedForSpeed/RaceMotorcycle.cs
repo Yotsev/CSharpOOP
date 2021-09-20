b7ea6ca079df6e187ea58d908e1bacf68dc9caec
Yotsev <b.yotsev@gmail.com>
2021-09-20 12:17:47 +0300

    Added some of the tasks form inheritance

---
+++ 01.Inheritance/InheritanceExercise/NeedForSpeed/RaceMotorcycle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NeedForSpeed
+{
+    class RaceMotorcycle : Motorcycle
+    {
+        public RaceMotorcycle(int horsePower, double fuel)
+            : base(horsePower, fuel)
+        {
+            DefaultFuelConsumption = 8;
+        }
+
+        public override void Drive(double kilometers)
+        {
+            Fuel -= DefaultFuelConsumption  * kilometers;
+        }
+    }
+}
