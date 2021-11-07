6ac75fdc86f3794177d38022a24b2139d786e386
Yotsev <b.yotsev@gmail.com>
2021-11-07 09:31:38 +0200

    Added abstraction lab tasks

---
+++ 03.InterfacesAndAbstraction/InterfacesAndAbstractionLab/Cars/Seat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Seat : ICar
+    {
+        public string Model { get; }
+
+        public string Color { get; }
+
+        public Seat(string model, string color)
+        {
+            this.Model = model;
+            this.Color = color;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {GetType().Name} {this.Model}{Environment.NewLine}{this.Start()}{Environment.NewLine}{this.Stop()}";
+
+        }
+    }
+}
---
+++ 03.InterfacesAndAbstraction/InterfacesAndAbstractionLab/Cars/Tesla.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    partial class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; }
+
+        public string Color { get; }
+
+        public int Battery  { get; }
+
+        public Tesla(string mode, string color, int battery)
+        {
+            this.Model = mode;
+            this.Color = color;
+            this.Battery = battery;
+        }
+
+        public string Start()
+        {
+            return "Engine start";
+        }
+
+        public string Stop()
+        {
+            return "Breaaak!";
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Color} {this.GetType().Name} with {this.Battery} Batteries{Environment.NewLine}{this.Start()}{Environment.NewLine}{this.Stop()}";
+        }
+    }
+}
