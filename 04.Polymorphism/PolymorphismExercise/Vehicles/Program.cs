5a1a58b732b302532ca790d7fe7823354a482ff5
Yotsev <b.yotsev@gmail.com>
2021-11-14 10:45:38 +0200

    Added two task from polymorphism exercise

---
+++ 04.Polymorphism/PolymorphismExercise/Vehicles/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+
+namespace Vehicles
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] carInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] truckInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            string[] busInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            IVehicle car = new Car(double.Parse(carInfo[1]), double.Parse(carInfo[2]), double.Parse(carInfo[3]));
+            IVehicle truck = new Truck(double.Parse(truckInfo[1]), double.Parse(truckInfo[2]), double.Parse(truckInfo[3]));
+            IVehicle bus = new Bus(double.Parse(busInfo[1]), double.Parse(busInfo[2]), double.Parse(busInfo[3]));
+
+            int numberOfCommand = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfCommand; i++)
+            {
+                string[] inputInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                try
+                {
+                    if (inputInfo[0] == "Drive")
+                    {
+                        if (inputInfo[1] == "Car")
+                        {
+                            if (car.CanDrive(double.Parse(inputInfo[2])))
+                            {
+                                car.Drive(double.Parse(inputInfo[2]));
+                                Console.WriteLine($"Car travelled {double.Parse(inputInfo[2])} km");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Car needs refueling");
+                            }
+                        }
+                        else if (inputInfo[1] == "Truck")
+                        {
+                            if (truck.CanDrive(double.Parse(inputInfo[2])))
+                            {
+                                truck.Drive(double.Parse(inputInfo[2]));
+                                Console.WriteLine($"Truck travelled {double.Parse(inputInfo[2])} km");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Truck needs refueling");
+                            }
+                        }
+                        else if (inputInfo[1] == "Bus")
+                        {
+                            bus.IsEmpty = false;
+                            if (bus.CanDrive(double.Parse(inputInfo[2])))
+                            {
+                                bus.Drive(double.Parse(inputInfo[2]));
+                                Console.WriteLine($"Bus travelled {double.Parse(inputInfo[2])} km");
+                            }
+                            else
+                            {
+                                Console.WriteLine("Bus needs refueling");
+                            }
+                        }
+                    }
+                    else if (inputInfo[0] == "Refuel")
+                    {
+                        if (inputInfo[1] == "Car")
+                        {
+                            car.Refuel(double.Parse(inputInfo[2]));
+                        }
+                        else if (inputInfo[1] == "Truck")
+                        {
+                            truck.Refuel(double.Parse(inputInfo[2]));
+                        }
+                        else if (inputInfo[1] == "Bus")
+                        {
+                            bus.Refuel(double.Parse(inputInfo[2]));
+                        }
+                    }else if (inputInfo[0] == "DriveEmpty")
+                    {
+                        bus.IsEmpty = true;
+                        if (bus.CanDrive(double.Parse(inputInfo[2])))
+                        {
+                            bus.Drive(double.Parse(inputInfo[2]));
+                            Console.WriteLine($"Bus travelled {double.Parse(inputInfo[2])} km");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Bus needs refueling");
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            Console.WriteLine($"Car: {car.FuelQuantity:F2}");
+            Console.WriteLine($"Truck: {truck.FuelQuantity:F2}");
+            Console.WriteLine($"Bus: {bus.FuelQuantity:F2}");
+        }
+    }
+}
---
+++ 04.Polymorphism/PolymorphismExercise/Vehicles/Vehicle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Vehicles
+{
+    public abstract class Vehicle : IVehicle
+    {
+        private double fuelQuantity;
+        private double fuelConsumption;
+        private double tankCapacity;
+
+        public double TankCapacity
+        {
+            get => tankCapacity;
+            set => tankCapacity = value;
+        }
+
+        protected Vehicle(double fuelQuantity, double fuelConsumption, double tankCapacity)
+        {
+            this.TankCapacity = tankCapacity;
+            this.FuelQuantity = fuelQuantity;
+            this.FuelConsumption = fuelConsumption;
+        }
+
+        public double FuelQuantity
+        {
+            get => fuelQuantity;
+            set
+            {
+                if (value > this.TankCapacity)
+                {
+                    value = 0;
+                }
+
+                fuelQuantity = value;
+            }
+        }
+        public virtual double FuelConsumption
+        {
+            get => fuelConsumption;
+            set => fuelConsumption = value;
+        }
+        public bool IsEmpty { get; set; }
+
+        public bool CanDrive(double km)
+        {
+            if (this.FuelQuantity - (km * this.FuelConsumption) >= 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Drive(double km)
+        {
+            if (!this.CanDrive(km))
+            {
+                return;
+            }
+
+            this.FuelQuantity -= km * this.FuelConsumption;
+        }
+
+        public virtual void Refuel(double liters)
+        {
+            if (liters <= 0)
+            {
+                throw new ArgumentException("Fuel must be a positive number");
+            }
+
+            if (this.FuelQuantity + liters > this.TankCapacity)
+            {
+                throw new InvalidOperationException($"Cannot fit {liters} fuel in the tank");
+            }
+
+            this.FuelQuantity += liters;
+        }
+    }
+}
