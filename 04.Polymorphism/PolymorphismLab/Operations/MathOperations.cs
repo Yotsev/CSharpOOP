35c0ee464511ff962e967737c19cb6b60d04e9fc
Yotsev <b.yotsev@gmail.com>
2021-11-14 08:54:31 +0200

    Added polymorphism lab tasks

---
+++ 04.Polymorphism/PolymorphismLab/Operations/MathOperations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        public double Add(double firstNum, double seconNum, double thirdNum)
+        {
+            return firstNum + seconNum + thirdNum;
+        }
+
+        public decimal Add(decimal firrstNum, decimal secondNum, decimal thirdNum)
+        {
+            return firrstNum + secondNum + thirdNum;
+        }
+
+    }
+}
